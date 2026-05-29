f5778acad8f2aa72066e182528ff63e5c7039b9d
George Attaloglou <georgeeattaloglou@gmail.com>
2026-05-29 17:29:27 +0300

    add call_funtion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from prompts import system_prompt
+from call_function import available_functions
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -25,15 +26,26 @@ def main():
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
+    model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+
+    
 
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(response.text)
+        
+        if response.function_calls:
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
+        else:
+            print(response.text)
     else:
-        print(response.text)
+        if response.function_calls:
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
+        else:
+            print(response.text)
     
 
 
