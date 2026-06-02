2473c4fe7b3dc3151682175874e403ab31d0d140
George Attaloglou <georgeeattaloglou@gmail.com>
2026-06-02 18:06:36 +0300

    complete agent loop

--- main.py
+++ main.py
@@ -25,39 +25,52 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-        ),
-    )
 
-    if verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-    if not response.function_calls:
-        print(response.text)
-        return
-
-    function_results = []
-    for function_call in response.function_calls:
-        function_call_result = call_function(function_call, verbose)
-
-        if not function_call_result.parts:
-            raise Exception("Function Call Error (1): call_function should have a non-empty .parts list.")
-        if not function_call_result.parts[0].function_response:
-            raise Exception("Function Call Error (2): Empty function response list")
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception("Function Call Error (3): Empty function response field")
-
-        function_results.append(function_call_result.parts[0])
+    def generate_response():
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+            ),
+        )
+        return response
+    
+    for _ in range(20):
+        response = generate_response()
+        candidates = response.candidates
+        if candidates:
+            for candidate in candidates:
+                messages.append(candidate.content)
 
         if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        if not response.function_calls:
+            print(response.text)
+            break
+
+        function_results = []
+        for function_call in response.function_calls:
+            function_call_result = call_function(function_call, verbose)
+
+            if not function_call_result.parts:
+                raise Exception("Function Call Error (1): call_function should have a non-empty .parts list.")
+            if not function_call_result.parts[0].function_response:
+                raise Exception("Function Call Error (2): Empty function response list")
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception("Function Call Error (3): Empty function response field")
+
+            function_results.append(function_call_result.parts[0])
+
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+        messages.append(types.Content(role="user", parts=function_results))
+    else:
+        exit(1)
 
 
 if __name__ == "__main__":
